1693f432715fb8791b07f3ba0df1998ad87cc11f
animationpeg <jeffers800@hotmail.co.uk>
2026-01-10 13:38:49 +0000

    Initial program upload

--- Program.cs
+++ Program.cs
@@ -10,7 +10,11 @@ namespace DiceGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Press any key to roll the dice.");
+            }
+
         }
     }
 }
