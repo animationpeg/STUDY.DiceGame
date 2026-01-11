4e144c6753e74d9024e7e850a63b765e25bc7155
animationpeg <jeffers800@hotmail.co.uk>
2026-01-11 10:48:50 +0000

    Dice game. Roll the dice (random number 1-6) 10 times against the program. Program keeps track of score and a winner is declared at the end.

--- Program.cs
+++ Program.cs
@@ -10,11 +10,59 @@ namespace DiceGame
     {
         static void Main(string[] args)
         {
+            int playerRandomNum;
+            int enemyRandomNum;
+
+            int playerPoints = 0;
+            int enemyPoints = 0;
+
+            Random random = new Random();
+
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine("Press any key to roll the dice.");
+                Console.ReadKey();
+                playerRandomNum = random.Next(1, 7);
+                Console.WriteLine($"You rolled a {playerRandomNum}");
+
+                Console.WriteLine("...");
+                System.Threading.Thread.Sleep(1000); // Pauses program for 1000ms
+
+                enemyRandomNum = random.Next(1, 7);
+                Console.WriteLine($"Enemy AI rolled a {enemyRandomNum}");
+
+                if (playerRandomNum > enemyRandomNum)
+                {
+                    playerPoints++;
+                    Console.WriteLine("Player wins this round");
+                }
+                else if (enemyRandomNum > playerRandomNum)
+                {
+                    enemyPoints++;
+                    Console.WriteLine("Enemy wins this round");
+                }
+                else
+                {
+                    Console.WriteLine("draw!");
+                }
+                Console.WriteLine($"The score is now - Player: {playerPoints}. Enemy: {enemyPoints}");
+                Console.WriteLine("");
             }
 
+            // Declare winner of the dice game
+            if (playerPoints > enemyPoints)
+            {
+                Console.WriteLine("You win!");
+            }
+            else if (enemyPoints > playerPoints)
+            {
+                Console.WriteLine("Enemy wins!");
+            }
+            else
+            {
+                Console.WriteLine("Game ends in a draw!");
+            }
+            Console.ReadKey();
         }
     }
 }
